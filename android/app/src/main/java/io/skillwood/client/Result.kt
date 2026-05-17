9b640bb2ca669b79126e4839b1560befbc51b30c
Ivan <ivan@ya.ru>
2026-05-17 09:47:07 +0300

    APK в репозиторий

--- android/app/src/main/java/io/skillwood/client/Result.kt
+++
@@ -1,8 +0,0 @@
-package io.skillwood.client
-
-sealed class Result<out T> {
-    data class Success<T>(val value: T) : Result<T>()
-    data class Error(val message: String, val kind: ErrorKind) : Result<Nothing>()
-
-    enum class ErrorKind { Network, Unauthorized, NotFound, BadRequest, Server, Unknown }
-}
